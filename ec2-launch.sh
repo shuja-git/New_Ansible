09a8764523d45555ce5a90d82efc599bcceb1dd6
shuja <shuja.devops@gmail.com>
2022-04-06 23:14:13 +0530

    create instance from launch template

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#
 #aws ec2 request-spot-instances --instance-count 1 --type "persistent" --launch-specification file://record.json --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=frontend}]" | jq
 
 TEMP_ID="lt-0041090f0ef82990e"

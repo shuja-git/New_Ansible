a968e0f5178bca49b758fcdb7b7aec42444c7874
shuja <shuja.devops@gmail.com>
2022-04-06 17:25:07 +0530

    tags for spot request

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws ec2 request-spot-instances --instance-count 1 --type "persistent" --launch-specification file://record.json --tag-specifications ResourceType=spot-instances-request,Tags=[{Key=Name,Value=frontend} | jq
+aws ec2 request-spot-instances --instance-count 1 --type "persistent" --launch-specification file://record.json --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=frontend}" | jq

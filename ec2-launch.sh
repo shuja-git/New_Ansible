aee7b08759d79c30d17349c86834489fd1306db7
shuja <shuja.devops@gmail.com>
2022-04-07 15:40:38 +0530

    rec.json fix

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -8,9 +8,6 @@ if [ -z "$1" ]; then
 fi
 COMPONENT=$1
 
-TEMP_ID="lt-0041090f0ef82990e"
-VER=6
-ZONE_ID=Z02080483A5K2UWOFAMM5
 
 
 aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].State.Name | sed 's/"//g' | grep -E 'running|stopped' &>/dev/null
@@ -23,10 +20,14 @@ fi
 
 aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${VER} --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}]" "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
 
+TEMP_ID="lt-0041090f0ef82990e"
+VER=6
+ZONE_ID=Z02080483A5K2UWOFAMM5
+
 
 IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].PrivateIpAddress | sed 's/"//g' | grep -v null )
 
 #Update the DNS record
-sed -e "s/IPADDRESS/${IPADDRESS}/" -e "s/COMPONENT/${COMPONENT}/" rec.json &>/tmp/record.json
+sed -e "s/IPADDRESS/${IPADDRESS}/" -e "s/COMPONENT/${COMPONENT}/" rec.json >/tmp/record.json
 
 aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq

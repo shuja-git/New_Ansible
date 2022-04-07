8f6dabcdb74d5b6938ca21681e7517ba182fcabf
shuja <shuja.devops@gmail.com>
2022-04-07 12:51:52 +0530

    validate input is there

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -2,8 +2,13 @@
 #
 #aws ec2 request-spot-instances --instance-count 1 --type "persistent" --launch-specification file://record.json --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=frontend}]" | jq
 
+if [ -z "$1" ]; then
+    echo -e "\e[1;31mInpute is missing"
+    exit 1
+fi
+COMPONENT=$1
 
-aws ec2 describe-instances --filters "Name=tag:Name,Values=frontend" | jq .Reservations[].Instances[].State.Name | sed 's/"//g' | grep -E 'running|stopped' &>/dev/null
+aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].State.Name | sed 's/"//g' | grep -E 'running|stopped' &>/dev/null
 if [ $? -eq 0 ]; then
   echo -e "\e[1;31mInstance already there"
   exit
@@ -11,4 +16,4 @@ fi
 
 TEMP_ID="lt-0041090f0ef82990e"
 VER=6
-aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${VER} --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=frontend}]" "ResourceType=instance,Tags=[{Key=Name,Value=frontend}]" | jq
+aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${VER} --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}]" "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}]" | jq
